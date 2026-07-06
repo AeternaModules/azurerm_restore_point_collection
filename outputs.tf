f548290380e03b0988f05dfecacc4222744a77c7
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:14:44 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,24 @@ output "restore_point_collections" {
   description = "All restore_point_collection resources"
   value       = azurerm_restore_point_collection.restore_point_collections
 }
+output "restore_point_collections_location" {
+  description = "List of location values across all restore_point_collections"
+  value       = [for k, v in azurerm_restore_point_collection.restore_point_collections : v.location]
+}
+output "restore_point_collections_name" {
+  description = "List of name values across all restore_point_collections"
+  value       = [for k, v in azurerm_restore_point_collection.restore_point_collections : v.name]
+}
+output "restore_point_collections_resource_group_name" {
+  description = "List of resource_group_name values across all restore_point_collections"
+  value       = [for k, v in azurerm_restore_point_collection.restore_point_collections : v.resource_group_name]
+}
+output "restore_point_collections_source_virtual_machine_id" {
+  description = "List of source_virtual_machine_id values across all restore_point_collections"
+  value       = [for k, v in azurerm_restore_point_collection.restore_point_collections : v.source_virtual_machine_id]
+}
+output "restore_point_collections_tags" {
+  description = "List of tags values across all restore_point_collections"
+  value       = [for k, v in azurerm_restore_point_collection.restore_point_collections : v.tags]
+}
 
